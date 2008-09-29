586003a6aade0be1f23f6b4537d6b8fb8bdb6d70
Rune V. Sjoen <rune.sjoen@cern.ch>
2008-09-29 14:32:05 +0200

    Renamed some python modules

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 from distutils.core import setup
-setup(name="sftoolkit", version="1.0", py_modules=["sftoolkit"])
+setup(name="sflowtoolkit", version="1.0", py_modules=["sflowtoolkit","sflowdaemon"])
