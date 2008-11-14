27e574ae1acc6a2865c19b5392decde212d6b174
Rune V. Sjoen <rune.sjoen@cern.ch>
2008-11-14 15:19:18 +0100

    Put the db handle on the stack

--- src/storage.c
+++ src/storage.c
@@ -1,24 +1,24 @@
 #include "storage.h"
 
-MYSQL* db;
+MYSQL db;
 
 void storage_error()
 {
-		logmsg(LOGLEVEL_ERROR, "storage: %s", mysql_error(db));
+		logmsg(LOGLEVEL_ERROR, "storage: %s", mysql_error(&db));
 		exit_on_error();
 }
 
 void storage_init()
 {
-	if(!mysql_init(db))
+	if(!mysql_init(&db))
 		storage_error();
-	if(!mysql_real_connect(db, DB_HOST, DB_USER, DB_PASS, DB_NAME, 0, NULL, 0))
+	if(!mysql_real_connect(&db, DB_HOST, DB_USER, DB_PASS, DB_NAME, 0, NULL, 0))
 		storage_error();
 }
 
 void storage_destroy()
 {
-	mysql_close(db);
+	mysql_close(&db);
 }
 
 void storage_store_conv_ethernet(conv_key_ethernet_t* key, conv_ethernet_t* conv)
