0361216270171d314a863e4eeaef8fb8f8a2ed4e
Rune V. Sjoen <rune.sjoen@cern.ch>
2009-04-17 11:38:12 +0200

    rrd storage module does nothing for now

--- src/storage_rrd.c
+++ src/storage_rrd.c
@@ -1,15 +1,11 @@
 #include "storage.h"
 #include "storage_rrd.h"
 
-void storage_rrd_store_cntr(counter_list_t* list){
-
-}
-
 storage_module_t storage_mod_rrd = {
 	.name 					= "rrd",
 	.init 					= NULL,
 	.destroy 				= NULL,
-	.store_cntr 			= storage_rrd_store_cntr,
+	.store_cntr 			= NULL,
 	.store_conv_ethernet 	= NULL,
 	.store_conv_ip 			= NULL,
 	.store_conv_tcp 		= NULL,
--- src/storage_rrd.h
+++ src/storage_rrd.h
@@ -2,6 +2,5 @@
 #define __storage_rrd_h__
 
 void storage_rrd_load();
-void storage_rrd_store_cntr(counter_list_t* list);
 
 #endif
