7c80da1ec3651a00873ada4c5378152be7d1fa9a
Rune V. Sjoen <rune.sjoen@cern.ch>
2009-04-17 11:05:24 +0200

    Implement real writing of counter values in csv format

--- src/storage_csv.c
+++ src/storage_csv.c
@@ -5,12 +5,55 @@ time_t storage_csv_time;
 uint32_t storage_csv_fd = 0;
 
 void storage_csv_store_cntr(counter_list_t* list, uint32_t timestamp){
-	storage_csv_fd = shm_open("foobar", O_RDWR | O_CREAT, S_IRWXU);
-	char buf[];
-	uint32_t num;
-	num = sprintf(buf, ";;;;;;\n");
-	write(storage_csv_fd, buf, num);
-	close(storage_csv_fd);
+
+	// If we have no file handle
+	if (storage_csv_fd == 0){
+		char filename[32];
+		sprintf(filename, "counters_csv_%u", time(NULL));
+		storage_csv_fd = shm_open(filename, O_RDWR | O_CREAT, S_IRWXU);
+		storage_csv_time = time(NULL);
+	}
+
+	// If time is expired we need to create a new file handle
+	if( storage_csv_time % (60*5) == 0 ){
+		close(storage_csv_fd);
+		char filename[32];
+		sprintf(filename, "counters_csv_%u", time(NULL));
+		storage_csv_fd = shm_open(filename, O_RDWR | O_CREAT, S_IRWXU);
+		storage_csv_time = time(NULL);
+	}
+
+	char buf[1024];
+	counter_list_node_t* node = list->data;
+	while(node != NULL){
+		SFCntrSample* s = &node->sample;
+		char a[16];
+		num_to_ip(s->agent_address, a);
+		num = sprintf(buf,	"(%u, '%s', %u, %u, %llu, %u, %u, %llu, %u, %u, %u, %u, %u, %u, %llu, %u, %u, %u, %u, %u, %u)\n",
+				(uint32_t)s->timestamp,
+				a,
+				s->counter_generic_if_index,
+				s->counter_generic_if_type,
+				s->counter_generic_if_speed,
+				s->counter_generic_if_direction,
+				s->counter_generic_if_if_status,
+				s->counter_generic_if_in_octets,
+				s->counter_generic_if_in_ucast_pkts,
+				s->counter_generic_if_in_mcast_pkts,
+				s->counter_generic_if_in_bcast_pkts,
+				s->counter_generic_if_in_discards,
+				s->counter_generic_if_in_errors,
+				s->counter_generic_if_in_unknown_proto,
+				s->counter_generic_if_out_octets,
+				s->counter_generic_if_out_ucast_pkts,
+				s->counter_generic_if_out_mcast_pkts,
+				s->counter_generic_if_out_bcast_pkts,
+				s->counter_generic_if_out_discards,
+				s->counter_generic_if_out_errors,
+				s->counter_generic_if_promisc
+		);
+		write(storage_csv_fd, buf, num);
+	}
 }
 
 storage_module_t storage_mod_csv = {
